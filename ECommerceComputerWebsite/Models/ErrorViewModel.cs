6bbc4576f86e385066f76bd98de9977912f104d7
Nho094 <ngwxanhphannho@gmail.com>
2026-06-08 13:55:11 +0700

    1

---
+++ ECommerceComputerWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace ECommerceComputerWebsite.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
