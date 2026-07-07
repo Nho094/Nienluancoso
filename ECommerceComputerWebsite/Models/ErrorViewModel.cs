13ec51ec1f83da908db5c714a7347b2ef60fa307
Nho094 <ngwxanhphannho@gmail.com>
2026-07-07 13:07:52 +0700

    1

--- ECommerceComputerWebsite/Models/ErrorViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace ECommerceComputerWebsite.Models;
-
-public class ErrorViewModel
-{
-    public string? RequestId { get; set; }
-
-    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-}
